21e7d28013705b53ded43f3d7b64a2606cafd0e0
Yevhenii <155908667+jooaasss@users.noreply.github.com>
2024-05-19 17:33:06 +0100

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const body = document.querySelector('.page__body');
+const menuButton = document.querySelector('.icon--menu');
+const closeButton = document.querySelector('.icon--close');
+
+function toggleMenu() {
+  if (body.classList.contains('page__body--menu-opened')) {
+    body.classList.remove('page__body--menu-opened');
+  } else {
+    body.classList.add('page__body--menu-opened');
+  }
+}
+
+menuButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', toggleMenu);
